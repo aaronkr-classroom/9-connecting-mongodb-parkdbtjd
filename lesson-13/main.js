e14172e9e54ca1183c6b4ef11a7ac66194c36d62
parkdbtjd <163230755+parkdbtjd@users.noreply.github.com>
2024-05-01 05:23:02 +0000

    13끝!!!!!!

--- lesson-13/main.js
+++ lesson-13/main.js
@@ -6,10 +6,43 @@ const port = 3000,
   layouts = require("express-ejs-layouts"),
   homeController = require("./controllers/homeController"),
   errorController = require("./controllers/errorController"),
-  app = express();
+  app = express(),
+  MongoDB = require('mongodb').MongoClient,
+  dbURL = "mongodb+srv://dbtjdqkr12:dbtjd960713!@ut-node.9pkr7pn.mongodb.net/?retryWrites=true&w=majority&appName=UT-Node",
+  dbName = "ut-node";
   // @TODO: 몽고DB 모듈의 요청
 
-// @TODO: 로컬 MongoDB 데이터베이스 서버 연결 설정
+// @TODO: Atlas MongoDB 데이터베이스 서버 연결 설정
+MongoDB.connect(dbURL, (error, clinet) => {
+  if (error) throw error;
+
+  let db = clinet.db(dbName); // ut-node
+  db.collection("contacts")
+    .find()
+    .toArray((error, data) => { //contacts 컬렉션
+      if (error) throw error;
+      console.log(data);
+    });
+
+    db.collection("contacts")
+      .insertOne({
+        name: "Andrew",
+        job: "Doctor",
+        location: "Seoul"
+      }, (error, result) => {
+        if (error) throw error;
+        console.log(result);
+      })
+})
+.then(() => {
+  console.log("DB connected!")
+})
+.catch((error) => {
+  console.log("DB connection FAILED!");
+  console.log(error);
+  process.exit(1);
+});
+
 
 
 app.set("port", process.env.PORT || port);
